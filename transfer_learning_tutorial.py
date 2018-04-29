3d195d13f8661349a3263889f0a18ef147fa6e45
Ankita Kalra <ankitakalra@Ankitas-MBP.wv.cc.cmu.edu>
2018-04-29 17:10:17 -0400

    batch size=1

--- transfer_learning_tutorial.py
+++ transfer_learning_tutorial.py
@@ -36,7 +36,7 @@ data_dir = 'SunData'
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
                                           data_transforms[x])
                   for x in ['train', 'val']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=2,
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=1,
                                              shuffle=True, num_workers=4)
               for x in ['train', 'val']}
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
